3b5f2d323047707e55c94be51467dfc9730766bd
himbra <brahimanougmar19@gmail.com>
2024-10-10 23:15:27 +0100

    adding create product test

--- product-service/src/test/java/dev/himbra/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/dev/himbra/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,43 @@
 package dev.himbra.productservice;
 
+import dev.himbra.productservice.model.Product;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+
+    @LocalServerPort
+    private Integer port;
+
+    static {
+        mongoDBContainer.start();
+    }
 
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() {
+        RestAssured.port=port;
+        Product product = new Product("1","Samsung","an amazing phone",2000);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(product)
+                .when()
+                .post("/api/product/create")
+                .then()
+                .statusCode(201)
+                .body("name", Matchers.equalTo("Samsung"))
+                .body("price", Matchers.equalTo(2000.0F));
+
     }
 
 }
