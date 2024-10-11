1d371adf82aec3747e96119ca08f5e105c31cbc0
himbra <brahimanougmar19@gmail.com>
2024-10-11 12:18:16 +0100

    create the order entity

---
+++ order-service/src/main/java/dev/himbra/orderservice/model/Order.java
@@ -0,0 +1,20 @@
+package dev.himbra.orderservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor
+@Data @Entity(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String orderNumber;
+    private float price;
+    private int quantity;
+}
