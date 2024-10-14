9ac858656cb21aeece00ff10ac657b25f38dce6e
himbra <brahimanougmar19@gmail.com>
2024-10-14 23:58:42 +0100

    checking the stock using feignclient

--- order-service/src/main/java/dev/himbra/orderservice/restClient/InventoryClient.java
+++ order-service/src/main/java/dev/himbra/orderservice/restClient/InventoryClient.java
@@ -1,2 +1,11 @@
-package dev.himbra.orderservice.restClient;public interface InventoryClient {
+package dev.himbra.orderservice.restClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "inventory",url = "http://loacalhost:8082/api/inventory")
+public interface InventoryClient {
+    @GetMapping("/checkStock/{productCode}/{quantity}")
+    Boolean isInStock(@PathVariable String productCode, @PathVariable int quantity);
 }
